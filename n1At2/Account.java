f999dac589f8e442929c9cf050b0fc3f4a20c075
dantoso <80010223+dantoso@users.noreply.github.com>
2024-04-17 12:11:59 -0300

    feat: acc methods

--- n1At2/Account.java
+++ n1At2/Account.java
@@ -1,5 +1,30 @@
 package n1At2;
 
 public class Account {
-    
+    private float money;
+
+    public float getMoney() {
+        return money;
+    }
+
+    public void deposit(float value) {
+        money += value;
+    }
+
+    public float retrieve(float value) {
+        if(money - value < 0) {
+            float retrieval = money;
+            money = 0;
+            return retrieval;
+        }
+        
+        money -= value;
+        return value;
+    }
+
+    public float retrieveAll() {
+        float value = money;
+        money = 0;
+        return value;
+    }
 }
