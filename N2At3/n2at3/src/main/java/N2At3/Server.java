d455f9cc1718bcaee814c92f9c470f2cd260dea8
Caio Lucas Carneiro <128489415+CaioLucas1@users.noreply.github.com>
2024-06-16 13:28:23 -0300

    Server

---
+++ N2At3/n2at3/src/main/java/N2At3/Server.java
@@ -0,0 +1,161 @@
+package N2At3;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    private String filename = "livros.json";
+    private ServerSocket server;
+    private Socket socket;
+
+    private InputStream input;
+    private OutputStream out;
+
+    private File database = new File(filename);
+
+    public void start(int port) {
+        try {
+            server = new ServerSocket(port);
+            System.out.println("Server started");
+            System.out.println("Waiting for a client ...");
+
+            socket = server.accept();
+            input = socket.getInputStream();
+            out = socket.getOutputStream();
+
+            System.out.println("Connection established");
+
+            sendData();
+            waitForResponse();
+
+        } catch(Exception e) {
+            System.out.println("Server failed to start");
+        }
+    }
+
+    private void waitForResponse() {
+        try {
+            byte[] buffer = new byte[1024];
+            int responseLen = input.read(buffer);
+            String response = new String(buffer, 0, responseLen);
+
+            Character command = response.charAt(0);
+            String data = response.substring(1);
+
+            switch(command) {
+                case 'N':
+                    registerBook(data);
+                    break;
+                case 'A':
+                    rentBook(data);
+                    break;
+                case 'D':
+                    returnBook(data);
+                    break;
+                default:
+                    break;
+            }
+
+        } catch(Exception e) {
+            System.out.println("Server failed to read input");
+            e.printStackTrace();
+        }
+    }
+
+    private void registerBook(String data) {
+        Book newBook = BookParser.toBook(data);
+
+        String databaseString = scanFile();
+        Book[] list = BookParser.toBookList(databaseString);
+
+        Book[] newList = new Book[list.length + 1];
+        for(int i = 0; i<list.length; i++) {
+            newList[i] = list[i];
+        }
+        newList[list.length] = newBook;
+
+        saveFile(newList);
+        sendData();
+        System.out.println("Novo livro registrado, rode o programa novamente ou veja as mudancas em " + filename);
+    }
+
+    private void rentBook(String data) {
+        int index = Integer.parseInt(data);
+        String databaseString = scanFile();
+
+        Book[] list = BookParser.toBookList(databaseString);
+        list[index].takeCopies(1);
+
+        saveFile(list);
+        System.out.println("Livro alugado, rode o programa novamente ou veja as mudancas em " + filename);
+    }
+
+    private void returnBook(String data) {
+        int index = Integer.parseInt(data);
+        String databaseString = scanFile();
+
+        Book[] list = BookParser.toBookList(databaseString);
+        list[index].returnCopies(1);
+
+        saveFile(list);
+        System.out.println("Livro devolvido, rode o programa novamente ou veja as mudancas em " + filename);
+    }
+
+    private void sendData() {
+        String data = scanFile();
+        byte[] bytes = data.getBytes();
+        try {
+            out.write(bytes);
+            out.flush();
+        } catch (Exception e) {
+            System.out.println("Server failed to send data");
+        }
+    }
+
+    private String scanFile() {
+        String databaseString = null;
+
+        try {
+            Scanner scanner = new Scanner(database);
+
+            databaseString = "";
+            while(scanner.hasNextLine()) {
+                databaseString += scanner.nextLine();
+            }
+            
+            scanner.close();
+        } catch(Exception e) {
+            System.out.println("Failed to read livros.json file");
+            e.printStackTrace();
+        }
+
+        return databaseString;
+    }
+
+    private void saveFile(Book[] list) {
+        String newData = BookParser.toString(list);
+        database.delete();
+        database = new File(filename);
+        try {
+            database.createNewFile();
+            FileWriter writer = new FileWriter(filename);
+            writer.write(newData);
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("failed to save data");
+        }
+    }
+
+    public void saveFromString(String data) {
+        try {
+            database.createNewFile();
+            FileWriter writer = new FileWriter(filename);
+            writer.write(data);
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("failed to save data");
+        }
+    }
+}
